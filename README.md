66057f705c854619b380314694cd09843d4cf593
MHntl <mh.hantal@gmail.com>
2023-09-18 09:44:39 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# QuoteGen React
+
+<h1>Javascript Quote Generator App</h1>
+
+<h4>React have been used in this project.</h4>
+
+<img src="./screen.gif">
---
+++ src/App.js
@@ -0,0 +1,54 @@
+import "./App.css";
+import "./style.css";
+import React, { useState } from "react";
+
+function App() {
+  const [minVal, setMinVal] = useState(0);
+  const [maxVal, setMaxVal] = useState(10);
+  const [randomNum, setRandomNum] = useState(5);
+
+  function getRandomNumber() {
+    setRandomNum(
+      Math.floor(Math.random() * (maxVal - minVal + 1)) + minVal
+    );
+  }
+
+  return (
+    <div className="hero">
+      <div className="container">
+        <div className="randomNum">
+          <p style={{ color: "white" }}>
+            Random Number : <span style={{ color: "yellow" }}>{randomNum}</span>
+          </p>
+        </div>
+        <div className="numContainer">
+          <div>
+            <p>Min:</p>
+            <input
+              type="number"
+              value={minVal}
+              onChange={(e) => {
+                setMinVal(parseInt(e.target.value));
+              }}
+            />
+          </div>
+          <div>
+            <p>Max:</p>
+            <input
+              type="number"
+              value={maxVal}
+              onChange={(e) => {
+                setMaxVal(parseInt(e.target.value));
+              }}
+            />
+          </div>
+        </div>
+        <button style={{ color: "white" }} onClick={getRandomNumber}>
+          Get Random Number
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
